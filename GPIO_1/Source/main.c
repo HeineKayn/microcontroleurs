781901212a5349b3ce9838ee52ab04bdc43f16da
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2021-09-17 17:32:50 +0200

    Controle de la LED verte avec le bouton bleu

--- GPIO_1/Source/main.c
+++ GPIO_1/Source/main.c
@@ -1,4 +1,18 @@
-
-int main () {
-	while(1);
-}
+#include "stm32f10x.h"
+int main(void){
+	RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
+	
+	GPIOA->CRL &= ~(0xf << 5*4);
+	GPIOA->CRL |= (2 << 5*4);
+	
+	GPIOC->CRH &= ~(0xf << 5*4);
+	GPIOC->CRH |= (4 << 5*4);
+	while(1) {
+		if (((GPIOC->IDR >> 13) & 1) == 0) {
+			GPIOA->ODR |= (1 << 5);
+		} 
+		else {
+			GPIOA->ODR &= ~(1 << 5);
+		}
+	}
+}	
