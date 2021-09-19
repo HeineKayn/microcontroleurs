39bb5b8bea397307cf7bd29e929368336f859c23
Hugo Barral <hugo.barral@outlook.fr>
2021-09-19 21:20:10 +0200

    Ajout de commentaires

--- GPIO_1/Source/main.c
+++ GPIO_1/Source/main.c
@@ -10,10 +10,13 @@ int main(void){
 	GPIO_Init(GPIOC, 13, In_Floating);
 
 	while(1) {
+		// En cas d'appuie sur le bouton (ODR == 0)
 		if (GPIO_Read(GPIOC, 13) == 0) {
+			// On allume la LED (ODR = 1)
 			GPIO_Set(GPIOA, 5);
 		} 
 		else {
+			// Sinon on l'éteint (ODR = 0)
 			GPIO_Reset(GPIOA, 5);
 		}
 	}
