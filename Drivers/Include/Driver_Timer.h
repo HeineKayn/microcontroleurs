d132744d697c533db2e63e1fc2b98282fc05d5f8
Hugo Barral <hugo.barral@outlook.fr>
2021-09-24 15:16:26 +0200

    Ajout de commentaires au projet Timer et aux drivers

--- Drivers/Include/Driver_Timer.h
+++ Drivers/Include/Driver_Timer.h
@@ -2,9 +2,14 @@
 #define DRIVER_TIMER_H__
 #include "stm32f10x.h"
 
+// Initialisation du Timer avec une valeur d'autoreload (ARR) et de prescaler (PSC)
 void Timer_Base_Init (TIM_TypeDef * Timer, unsigned short ARR, unsigned short PSC);
+// Lancement du Timer
 void Timer_Base_Start (TIM_TypeDef * Timer);
+// Arrêt du Timer
 void Timer_Base_Stop (TIM_TypeDef * Timer);
+// Activation de l'interruption du Timer
+// Place la priorité (Prio) et défini un handler (IT_function)
 void Timer_ActiveIT(TIM_TypeDef * Timer, char Prio, void(*IT_function )(void));
 
 #endif
--- Timer/Source/main.c
+++ Timer/Source/main.c
@@ -1,17 +1,23 @@
 #include "Driver_Timer.h"
 #include "Driver_GPIO.h"
 
+// Handler d'interruption
+// Inverse l'état de la LED
 void callback() {
 	GPIO_Toggle(GPIOA, 5);
 }
 
 
 int main(void){
+	// Initialisation de la GPIO de la LED
 	GPIO_Init(GPIOA, 5, Out_Ppull_2MHZ);
 	
+	// Initialisation du Timer 2 (500ms)
 	Timer_Base_Init(TIM2, 44999, 799);
+	// Activation de l'interruption du Timer 2 (priorité max)
 	Timer_ActiveIT(TIM2, 0, callback);
 	
+	// Lancement du Timer 2
 	Timer_Base_Start(TIM2);
 	
 	while(1);
