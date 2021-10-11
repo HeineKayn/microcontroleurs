4309454825fb538f132c2b945778006b53bb106c
Hugo Barral <hugo.barral@outlook.fr>
2021-10-11 23:51:48 +0200

    Fix driver Timer : cannot use more than 4 timers

--- Drivers/Source/Driver_Timer.c
+++ Drivers/Source/Driver_Timer.c
@@ -14,8 +14,10 @@ void Timer_Base_Init (TIM_TypeDef * Timer, unsigned short ARR, unsigned short PS
 		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	else if (Timer == TIM3)
 		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-	else
+	else if (Timer == TIM4)
 		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	else
+		return;
 	
 	Timer->PSC = PSC;
 	Timer->ARR = ARR;
@@ -31,7 +33,7 @@ void Timer_Base_Stop (TIM_TypeDef * Timer) {
 
 
 void Timer_ActiveIT(TIM_TypeDef * Timer, char Prio, void(*IT_function )(void)) {
-	// Autorisation de l'interruption et miseneplace de la priorité dans le NVIC
+	// Autorisation de l'interruption et mise en place de la priorité dans le NVIC
 	// Attribution de l'adresse de l'interrupteur qau ointeur de fonction correspondant
 	if (Timer == TIM1) {
 		NVIC->ISER[0]|= 1 << 25;
@@ -45,10 +47,12 @@ void Timer_ActiveIT(TIM_TypeDef * Timer, char Prio, void(*IT_function )(void)) {
 		NVIC->ISER[0] = 1 << 29;
 		NVIC->IP[29] = (Prio << 4);
 		Timer3_Interupt = IT_function;
-	} else {
+		} else if (Timer == TIM4) {
 		NVIC->ISER[0] = 1 << 30;
 		NVIC->IP[30] = (Prio << 4);
 		Timer4_Interupt = IT_function;
+	} else {
+		return;
 	}
 	
 	// Autorisation des demandes d'interruption 
@@ -85,6 +89,7 @@ void Timer_PWM(TIM_TypeDef * Timer, char Channel) {
 }
 
 void Timer_Set_Cycle_PWM(TIM_TypeDef * Timer, char Channel, char Percent) {
+	// Calcul du pourcentage de l'ARR
 	uint16_t CCR = (Percent*Timer->ARR)/100;
 	switch (Channel) {
 		case 1:
@@ -104,24 +109,28 @@ void Timer_Set_Cycle_PWM(TIM_TypeDef * Timer, char Channel, char Percent) {
 	}
 }
 
+// Fonction d'interruption de TIM1
 void TIM1_UP_IRQHandler (void) {
 	TIM1->SR &= ~(0x1);
 	if (Timer1_Interupt != 0)
 		(*Timer1_Interupt)();
 }
 
+// Fonction d'interruption de TIM2
 void TIM2_IRQHandler (void) {
 	TIM2->SR &= ~(0x1);
 	if (Timer2_Interupt != 0)
 		(*Timer2_Interupt)();
 }
 
+// Fonction d'interruption de TIM3
 void TIM3_IRQHandler (void) {
 	TIM3->SR &= ~(0x1);
 	if (Timer3_Interupt != 0)
 		(*Timer3_Interupt)();
 }
 
+// Fonction d'interruption de TIM4
 void TIM4_IRQHandler (void) {
 	TIM4->SR &= ~(0x1);
 	if (Timer4_Interupt != 0)
